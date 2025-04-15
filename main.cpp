3de0aabe9c82e7aac29584b5eb4c7553c3ce4b1a
unknown <s283814@studenti.polito.it>
2025-04-15 11:07:02 +0200

    lettura pesi e calcolo rendimenti

--- main.cpp
+++ main.cpp
@@ -28,7 +28,40 @@ n = stoi(line.substr(line.find(';')+ 1));
 //Terza riga: intestazione; la ignoriamo
 getline(inputFile, line);
 
+//Lettura dei pesi e rendimenti
+float w[100], r[100]; 
+for (int i = 0; i < n; ++i) {
+	getline(inputFile, line);
+	stringstream ss(line);
+	string w_str, r_str;
+	
+	getline(ss, w_str, ';');
+	getline(ss, r_str, ';');
+	
+	w[i] = stof(w_str);
+	r[i] = stof(r_str);
+	
+}
 
+inputFile.close();
+//Calcolo del rendimento e del valore finale
+float r_tot = 0.0;
+for (int i = 0; i < n; ++i)
+	r_tot += w[i] * r[i];
+
+float V = (1+r_tot) * S;
+//Scrittura su file result.txt e su schermo
+ofstream outputFile("result.txt");
+if (!outputFile) {
+	cerr << "Errore nell'apertura del file" ;
+	return 1;
+}
+//Formattazione dell'output
+outputFile<< fixed << setprecision(2);
+cout << fixed << setprecision(2);
+
+	
+	
 
 	
 	
